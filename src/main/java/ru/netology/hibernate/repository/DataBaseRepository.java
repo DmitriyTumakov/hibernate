b520380f0b95be92bd17d6e618bf7c70feb3fb79
Dmitriy Tumakov <dmit.tumakov@gmail.com>
2024-07-26 20:19:12 +0300

    Code improved

--- src/main/java/ru/netology/hibernate/repository/DataBaseRepository.java
+++ src/main/java/ru/netology/hibernate/repository/DataBaseRepository.java
@@ -3,10 +3,8 @@ package ru.netology.hibernate.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import ru.netology.hibernate.entities.PersonId;
 import ru.netology.hibernate.entities.Persons;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -17,16 +15,7 @@ public class DataBaseRepository {
     public DataBaseRepository() {}
 
     public List<Persons> getPersonsByCity(String city) {
-        List<Persons> result = new ArrayList<Persons>();
-        List<Persons> personsList = entityManager.createNativeQuery("select * from persons_schema.persons where city_of_living = '" + city + "'", Persons.class)
-                .getResultList();
-
-        for (Persons persons : personsList) {
-            if (persons.getCity_of_living().equals(city)) {
-                result.add(persons);
-            }
-        }
-
-        return result;
+        return entityManager.createQuery("from Persons where cityOfLiving like :city", Persons.class)
+                .setParameter("city", city).getResultList();
     }
 }
